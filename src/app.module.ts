1d4280680a86c91572c825dd8342c0f9af30c2ab
uiurihappy <uiurihappy@sju.ac.kr>
2023-01-09 17:04:22 +0900

    feat: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { Users } from '@Src/entities/Users.entity';
 import { UsersService } from '@Src/api/users/users.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { MiddlewareConsumer, Module, NestModule, Logger } from '@nestjs/common';
@@ -9,6 +10,15 @@ import { ChannelsModule } from './api/channels/channels.module';
 import { DmsModule } from './api/dms/dms.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { ChannelChats } from '@Src/entities/ChannelChats.entity';
+import { ChannelMembers } from '@Src/entities/ChannelMembers.entity';
+import { Channels } from '@Src/entities/Channels.entity';
+import { DMs } from '@Src/entities/DMs.entity';
+import { Mentions } from '@Src/entities/Mentions.entity';
+import { WorkspaceMembers } from '@Src/entities/WorkspaceMembers.entity';
+import { Workspaces } from '@Src/entities/Workspaces.entity';
 
 const getEnv = async () => {
   const response = await axios.get('/test');
@@ -18,6 +28,30 @@ const getEnv = async () => {
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.MYSQL_HOST,
+        port: 3306,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        synchronize: false,
+        entities: [
+          ChannelChats,
+          ChannelMembers,
+          Users,
+          Channels,
+          Workspaces,
+          WorkspaceMembers,
+          DMs,
+          Mentions,
+        ],
+        // keepConnectionAlive: true,
+        logging: ['error'],
+        timezone: '+09:00',
+      }),
+    }),
     UsersModule,
     WorkspacesModule,
     ChannelsModule,
---
+++ src/entities/Users.entity.ts
@@ -0,0 +1,96 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ChannelChats } from '@Src/entities/ChannelChats.entity';
+import { ChannelMembers } from '@Src/entities/ChannelMembers.entity';
+import { Channels } from '@Src/entities/Channels.entity';
+import { DMs } from '@Src/entities/DMs.entity';
+import { Mentions } from '@Src/entities/Mentions.entity';
+import { WorkspaceMembers } from '@Src/entities/WorkspaceMembers.entity';
+import { Workspaces } from '@Src/entities/Workspaces.entity';
+
+@Index('email', ['email'], { unique: true })
+@Entity({ schema: 'sleact', name: 'users' })
+export class Users {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('varchar', { name: 'email', unique: true, length: 30 })
+  email: string;
+
+  @Column('varchar', { name: 'nickname', length: 30 })
+  nickname: string;
+
+  @Column('varchar', { name: 'password', length: 100, select: false })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+
+  @OneToMany(() => ChannelChats, channelchats => channelchats.User)
+  ChannelChats: ChannelChats[];
+
+  @OneToMany(() => ChannelMembers, channelmembers => channelmembers.User)
+  ChannelMembers: ChannelMembers[];
+
+  @OneToMany(() => DMs, dms => dms.Sender)
+  DMs: DMs[];
+
+  @OneToMany(() => DMs, dms => dms.Receiver)
+  DMs2: DMs[];
+
+  @OneToMany(() => Mentions, mentions => mentions.Sender)
+  Mentions: Mentions[];
+
+  @OneToMany(() => Mentions, mentions => mentions.Receiver)
+  Mentions2: Mentions[];
+
+  @OneToMany(() => WorkspaceMembers, workspacemembers => workspacemembers.User)
+  WorkspaceMembers: WorkspaceMembers[];
+
+  @OneToMany(() => Workspaces, workspaces => workspaces.Owner)
+  OwnedWorkspaces: Workspaces[];
+
+  @ManyToMany(() => Workspaces, workspaces => workspaces.Members)
+  @JoinTable({
+    name: 'workspacemembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'WorkspaceId',
+      referencedColumnName: 'id',
+    },
+  })
+  Workspaces: Workspaces[];
+
+  @ManyToMany(() => Channels, channels => channels.Members)
+  @JoinTable({
+    name: 'channelmembers',
+    joinColumn: {
+      name: 'UserId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'ChannelId',
+      referencedColumnName: 'id',
+    },
+  })
+  Channels: Channels[];
+}
